dc60a4ba8a544d73396688a09cff1feeb4be5b15
CJMoye <Cjmoye30@gmail.com>
2023-04-03 22:07:01 -0400

    Promps.js file connected and working. Inquirer displaying and saving answers correctly

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js file which is going initiate all of the JS files that are going to be created
+const prompts = require("./assets/js/prompts")
+
+function init (){
+    prompts();
+}
+
+init();
