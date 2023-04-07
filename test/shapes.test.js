515b60c89aa622b66e757d164fa7910808f076c4
CJMoye <Cjmoye30@gmail.com>
2023-04-06 21:54:44 -0400

    MVP met! All tests are passing and SVG logo is created based on acceptance criteria

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,16 +1,65 @@
 // Importing the Shapes.js file so we can run our tests
-const { describe } = require("yargs");
-const Circle = require('./circle');
-const Square = require('./square');
-const Triangle = require('./triangle');
+const { Circle, Triangle, Square } = require('../shapes.js')
 
-describe("Triangle", () => {
-    describe("Triangle", () => {
-        it("triangle testing ------ -", () => {
-            const shape = new Triangle();
-            
+// A testing suite for Circle
+describe('Circle', () => {
+    test('should render the correct dimensions', () => {
+        const text = "abc";
+        const fill = "blue";
+        const textFill = "yellow";
+        const svgReqs = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><path d="M10 10"/>`
 
-        })
-    })
-})
+        const expectedSVG = 
+        `
+        ${svgReqs}
+        <circle cx="150" cy="100" r="80" fill="${fill}"/>
+        <text x="150" y="125" font-size="50" text-anchor="middle" fill="${textFill}">${text}</text>
+        </svg>
+        `;
 
+        const shape = new Circle(text, fill, textFill);
+        expect(shape.render()).toEqual(expectedSVG);
+    });
+});
+
+// A testing suite for Triangle
+describe('Triangle', () => {
+    test('should render the correct dimensions', () => {
+        const text = "abc";
+        const fill = "blue";
+        const textFill = "yellow";
+        const svgReqs = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><path d="M10 10"/>`
+
+        const expectedSVG = 
+        `
+        ${svgReqs}
+        <polygon points="150, 18 244, 182 56, 182" fill="${fill}"/>
+        <text x="150" y="125" font-size="50" text-anchor="middle" fill="${textFill}">${text}</text>
+        </svg>
+        `;
+
+        const shape = new Triangle(text, fill, textFill);
+        expect(shape.render()).toEqual(expectedSVG);
+    });
+});
+
+// A testing suite for Square
+describe('Square', () => {
+    test('should render the correct dimensions', () => {
+        const text = "abc";
+        const fill = "blue";
+        const textFill = "yellow";
+        const svgReqs = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><path d="M10 10"/>`
+
+        const expectedSVG = 
+        `
+        ${svgReqs}
+        <rect width="100%" height="100%" fill="${fill}"/>
+        <text x="150" y="125" font-size="50" text-anchor="middle" fill="${textFill}">${text}</text>
+        </svg>
+        `;
+
+        const shape = new Square(text, fill, textFill);
+        expect(shape.render()).toEqual(expectedSVG);
+    });
+});
